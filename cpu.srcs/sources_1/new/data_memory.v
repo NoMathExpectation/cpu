f4e5bb74286e526f76ffe8c94b1d3db58298d7e3
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2023-05-17 11:19:21 +0800

    memory led out

--- cpu.srcs/sources_1/new/data_memory.v
+++ cpu.srcs/sources_1/new/data_memory.v
@@ -25,23 +25,40 @@ module data_memory(
     input cpu_write,
     input [13:0] cpu_addr,
     input [31:0] cpu_data_in,
-    input [31:0] cpu_data_out,
+    output [31:0] cpu_data_out,
     
     input uart_reset,
     input uart_clk,
     input uart_write,
     input [14:0] uart_addr,
     input [31:0] uart_data_in,
-    input uart_done
+    input uart_done,
+    
+    output reg [31:0] led_out = 32'b0
     );
+    parameter [13:0] led_addr = 14'b0;
+    
     wire cpu_mode = uart_reset | uart_done;
     wire uart_write_final = uart_write & uart_addr[14];
+    wire [13:0] uart_addr_final = uart_addr[13:0];
     
     data_memory_internal data_mem(
         .clka(cpu_mode ? ~cpu_clk : uart_clk),
         .wea(cpu_mode ? cpu_write : uart_write_final),
-        .addra(cpu_mode ? cpu_addr : uart_addr[13:0]),
+        .addra(cpu_mode ? cpu_addr : uart_addr_final),
         .dina(cpu_mode ? cpu_data_in : uart_data_in),
         .douta(cpu_data_out)
     );
+    
+    always @(negedge cpu_clk or posedge uart_clk) begin
+        if (cpu_mode) begin
+            if (~cpu_clk & cpu_write & cpu_addr == led_addr) begin
+                led_out <= cpu_data_in;
+            end
+        end else begin
+            if (uart_clk & uart_write_final & uart_addr_final == led_addr) begin
+                led_out <= uart_data_in;
+            end
+        end
+    end
 endmodule
