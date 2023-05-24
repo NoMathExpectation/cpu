4a3a35fc5d04dc09ede83ade23efcbd8e70d7b3c
CaraZinc <103182678+Cara-Zinc@users.noreply.github.com>
2023-05-25 00:24:32 +0800

    Improve control unit

--- cpu.srcs/sources_1/new/control_unit.v
+++ cpu.srcs/sources_1/new/control_unit.v
@@ -31,11 +31,11 @@ assign r_format = (instruction[31:26]==6'b000000)? 1'b1:1'b0;
 assign jump = (instruction[31:26]==6'b000010);
 assign jr = ((instruction[31:26]==6'b000000)&&(instruction[5:0]==6'b001000)) ? 1'b1 : 1'b0;
 
-assign branch = (instruction[31:26]==6'b000100);
-assign nbranch = (instruction[31:26]==6'b000101);
+assign branch = (instruction[31:26]==6'b000100)? 1'b1 : 1'b0;
+assign nbranch = (instruction[31:26]==6'b000101)? 1'b1 : 1'b0;
 assign mem2reg = (instruction[31:26]==6'b100100)||(instruction[31:26]==6'b100101)||(instruction[31:26]==6'b110000)||(instruction[31:26]==6'b100011);
 assign mem_write = (instruction[31:26]==6'b101011)||(instruction[31:26]==6'b101000)||(instruction[31:26]==6'b101001)||(instruction[31:26]==6'b111000);
-assign alu_src = (i_format)&&!(branch)&&!(nbranch);
+assign alu_src = (i_format)||(instruction[31:26]==6'b100011)||(instruciton[31:26]==6'b101011);
 
 assign reg_dst = r_format; 
 assign reg_write = (r_format || (instruction[31:26]==6'b100011) || jal || i_format) &&!(jr);
