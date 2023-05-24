9c259b298faf43dd90d99a5612dc10ae4b21bfcd
CaraZinc <103182678+Cara-Zinc@users.noreply.github.com>
2023-05-24 12:56:46 +0800

    MemOrIO improved

--- cpu.srcs/sources_1/new/MemOrIO.v
+++ cpu.srcs/sources_1/new/MemOrIO.v
@@ -19,19 +19,57 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+
+
 module MemOrIO( mRead, mWrite, ioRead, ioWrite,addr_in, addr_out, m_rdata, io_rdata, r_wdata, r_rdata, write_data, LEDCtrl, SwitchCtrl);
 input mRead; // read memory, fromController
 input mWrite; // write memory, fromController
 input ioRead; // read IO, from Controller
 input ioWrite; // write IO, from Controller
 input[31:0] addr_in; // from alu_result in ALU
-output[31:0] addr_out; // address to Data-Memory
+
 input[31:0] m_rdata; // data read from Data-Memory
-input[15:0] io_rdata; // data read from IO,16 bits
-output[31:0] r_wdata; // data to Decoder(register file)
 input[31:0] r_rdata; // data read from Decoder(register file)
-output reg[31:0] write_data; // data to memory or I/O£¨m_wdata, io_wdata£©output LEDCtrl; // LED Chip Select
+input[15:0] io_rdata; // data read from IO,16 bits
+
+output reg[31:0] r_wdata; // data to Decoder(register file)
+output [31:0] addr_out; // address to Data-Memory
+output reg[31:0] write_data; // data to memory or I/O??m_wdata, io_wdata??output LEDCtrl; // LED Chip Select
 output SwitchCtrl; // Switch Chip Select
 output LEDCtrl;
+
+assign addr_out= addr_in;
+
+// The data wirte to register file may be from memory or io. // While the data is from io, it should be the lower 16bit of r_wdata. 
+always @(*) begin
+
+    if(mRead==1'b1)begin
+        r_wdata <= m_rdata;
+    end
+    else if(ioRead==1'b1)begin
+        r_wdata <= {{16{1'b0}},io_rdata}; 
+    end
+end
+// Chip select signal of Led and Switch are all active high;
+
+assign LEDCtrl= (ioWrite==1'b1)?1'b1:1'b0;
+assign SwitchCtrl= (ioRead==1'b1)?1'b1:1'b0;
+
+always @* begin
+    if((mWrite==1'b1)||(ioWrite==1'b1)) begin
+        //wirte_data could go to either memory or IO. where is it from?
+        write_data <= (mWrite==1'b0)?{16'b0,r_rdata[15:0]}:r_rdata;
+    end    
+    else begin
+        write_data = 32'hZZZZZZZZ;
+    end    
+end
+
+
+
+
+
+
+
 
 endmodule
