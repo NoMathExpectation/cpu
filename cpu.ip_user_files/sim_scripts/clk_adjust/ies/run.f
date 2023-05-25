65c0c1b1e9788c0235a32a7e4c52809383638a6b
NoMathExpectation <85624722+NoMathExpectation@users.noreply.github.com>
2023-05-26 00:50:40 +0800

    apply .gitignore

--- cpu.ip_user_files/sim_scripts/clk_adjust/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../cpu.srcs/sources_1/ip/clk_adjust/clk_adjust_clk_wiz.v" \
-  "../../../../cpu.srcs/sources_1/ip/clk_adjust/clk_adjust.v" \
--endlib
--makelib ies_lib/xil_defaultlib \
-  glbl.v
--endlib
-
--- cpu.ip_user_files/sim_scripts/uart/ies/run.f
+++
@@ -1,9 +0,0 @@
--makelib ies_lib/xil_defaultlib \
-  "../../../../cpu.srcs/sources_1/ip/uart/uart_bmpg.v" \
-  "../../../../cpu.srcs/sources_1/ip/uart/upg.v" \
-  "../../../../cpu.srcs/sources_1/ip/uart/sim/uart.v" \
--endlib
--makelib ies_lib/xil_defaultlib \
-  glbl.v
--endlib
-
