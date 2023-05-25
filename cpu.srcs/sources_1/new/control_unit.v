fd156f6b826a084e133dcae75797fbdb2a1f3c78
CaraZinc <103182678+Cara-Zinc@users.noreply.github.com>
2023-05-25 15:38:48 +0800

    A small bug

--- cpu.srcs/sources_1/new/control_unit.v
+++ cpu.srcs/sources_1/new/control_unit.v
@@ -35,7 +35,7 @@ assign branch = (instruction[31:26]==6'b000100)? 1'b1 : 1'b0;
 assign nbranch = (instruction[31:26]==6'b000101)? 1'b1 : 1'b0;
 assign mem2reg = (instruction[31:26]==6'b100100)||(instruction[31:26]==6'b100101)||(instruction[31:26]==6'b110000)||(instruction[31:26]==6'b100011);
 assign mem_write = (instruction[31:26]==6'b101011)||(instruction[31:26]==6'b101000)||(instruction[31:26]==6'b101001)||(instruction[31:26]==6'b111000);
-assign alu_src = (i_format)||(instruction[31:26]==6'b100011)||(instruciton[31:26]==6'b101011);
+assign alu_src = (i_format)||(instruction[31:26]==6'b100011)||(instruction[31:26]==6'b101011);
 
 assign reg_dst = r_format; 
 assign reg_write = (r_format || (instruction[31:26]==6'b100011) || jal || i_format) &&!(jr);
